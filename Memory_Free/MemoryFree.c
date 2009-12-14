b837052f3ab7bade2aa02e0d1f93a8ad2ddca68e
unknown <Compaq_Owner@.(none)>
2009-12-14 22:19:43 +0000

    New Version of MemoryFree to allow checking of minimum free memory

---
+++ Memory_Free/MemoryFree.c
@@ -0,0 +1,46 @@
+ /*
+ * MemoryFree.c
+ * returns the number of free RAM bytes
+*/
+
+#include "WProgram.h"  
+#include "MemoryFree.h"
+
+extern unsigned int __data_start;
+extern unsigned int __data_end;
+extern unsigned int __bss_start;
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+int memory_min = 0x7FFF;
+
+int freeMemory()
+{
+  int free_memory;
+
+  if((int)__brkval == 0)
+     free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  else
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+
+  return free_memory;
+}
+ 
+int memoryMin()
+{
+  return memory_min;
+}
+
+void memoryCheck()
+{
+  int free_memory;
+
+  if((int)__brkval == 0)
+     free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  else
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+  if (free_memory < memory_min) memory_min = free_memory;
+
+
+  return;
+}
---
+++ Memory_Free/MemoryFree.h
@@ -0,0 +1,20 @@
+// memoryFree header
+
+#ifndef	MEMORY_FREE_H
+#define MEMORY_FREE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int freeMemory();
+int memoryMin();
+void memoryCheck();
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
+ 
+
