416ecb524a93f4b3957e58fd82effd87c1df9408
ribbotson <richard.ibbotson@btinternet.com>
2009-12-15 14:01:40 +0000

    Initial Versions of libraries with NAK changes

---
+++ usb_host/Max3421e.h
@@ -0,0 +1,38 @@
+/* MAX3421E functions */
+#ifndef _MAX3421E_H_
+#define _MAX3421E_H_
+
+
+#include <Spi.h>
+//#include <WProgram.h>
+#include "WProgram.h"
+#include "Max3421e_constants.h"
+
+class MAX3421E : public SPI {
+    // byte vbusState;
+    public:
+        MAX3421E( void );
+        byte getVbusState( void );
+        void toggle( byte pin );
+        static void regWr( byte, byte );
+        char * bytesWr( byte, byte, char * );
+        static void gpioWr( byte );
+        byte regRd( byte );
+        char * bytesRd( byte, byte, char * );
+        byte gpioRd( void );
+        boolean reset();
+        boolean vbusPwr ( boolean );
+        void busprobe( void );
+        void powerOn();
+        byte IntHandler();
+        byte GpxHandler();
+        byte Task();
+    private:
+        void init();
+    friend class Max_LCD;        
+};
+
+
+
+
+#endif //_MAX3421E_H_
